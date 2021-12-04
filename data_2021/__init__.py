f9e084c5cabdcb57c17ed978311a0f62bd61e6a8
Dominick_Laptop <d.jamesjux@gmail.com>
2021-12-04 20:29:55 +0100

    Heutige Aufgaben...

--- data_2021/__init__.py
+++ data_2021/__init__.py
@@ -1,3 +1,5 @@
+import math
+
 from help_methods import openAsList
 
 
@@ -154,3 +156,164 @@ def tag2021_3_b():
     o2 = int(least_common[0], 2)
     print("CO2: {}, O2: {}".format(co2, o2))
     return co2 * o2
+
+
+class Bingo:
+    def __init__(self, lines):
+        self.position = {}
+        self.spielbrett= [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+        self.bingo = {
+            "row": [0, 0, 0, 0, 0],
+            "col": [0, 0, 0, 0, 0],
+        }
+        self.erstelleBoard(lines)
+
+    def __repr__(self):
+        return "{}".format(self.spielbrett)
+
+    def erstelleBoard(self, lines):
+        lines_new = []
+        for line in lines:
+            lines_new.append(line.split())
+        lines = lines_new
+
+        for i in range(5):
+            for j in range(5):
+                choice = int(lines[i][j])
+                self.spielbrett[i][j] = choice
+                self.position[choice] = (i, j)
+
+    def updateBoard(self, val):
+        try:
+            x, y = self.position[val]
+            self.updateBingo(x, y)
+        except KeyError:
+            pass
+
+    def updateBingo(self, x, y):
+        self.bingo["row"][x] += 1
+        self.bingo["col"][y] += 1
+
+    def checkRow(self):
+        for idx in range(5):
+            if self.bingo["row"][idx] == 5:
+                return idx
+        return None
+
+    def checkRows(self):
+        return self.checkRow() is not None
+
+    def checkCol(self):
+        for idx in range(5):
+            if self.bingo["col"][idx] == 5:
+                return idx
+        return None
+
+    def checkCols(self):
+        return self.checkCol() is not None
+
+    def checkBingo(self):
+        return self.checkCols() or self.checkRows()
+
+    def getWinNumbers(self):
+        if self.checkRows():
+            return self.spielbrett[self.checkRow()]
+        else:
+            list = []
+            for idx in range(0, len(self.spielbrett)):
+                list.append(self.spielbrett[idx][self.checkCol()])
+            return list
+
+
+def findWinnigBoard_a(bingo_felder, numbers):
+    for zahl in numbers:
+        for feld in bingo_felder:
+            feld.updateBoard(zahl)
+            if feld.checkBingo():
+                return feld, zahl
+
+
+def getWinningTime(idx, feld, numbers):
+    for jdx, zahl in enumerate(numbers):
+        feld.updateBoard(zahl)
+        if feld.checkBingo():
+            return {"idx": idx, "feld": feld, "anzahl": jdx, "zahl": zahl}
+
+
+def findLastBoard(bingo_felder, numbers):
+    werte = []
+    for idx in range(0, len(bingo_felder)):
+        werte.append(getWinningTime(idx, bingo_felder[idx], numbers))
+    print(werte)
+    werte.sort(key=lambda x: x.get('anzahl'), reverse=True)
+    print(werte)
+    return werte[0]
+
+
+def numberline_to_array(line):
+    number_arr = line.split(",")
+    numbers = []
+    for number in number_arr:
+        numbers.append(int(number))
+    return numbers
+
+
+def createBingoFelder(line_list):
+    bingo_felder = []
+    for idx in range(0, len(line_list), 6):
+        lines = []
+        for jdx in range(5):
+            lines.append(line_list[idx + jdx])
+        feld = Bingo(lines)
+        bingo_felder.append(feld)
+    return bingo_felder
+
+
+def getGespielteZahlen(numbers, letzte_zahl):
+    gespielteZahlen = []
+    for zahl in numbers:
+        gespielteZahlen.append(zahl)
+        if zahl == letzte_zahl:
+            return gespielteZahlen
+
+
+def tag2021_4(aufgabenteil):
+    inputpath = "data_2021/inputs/input_2021_4.txt"
+    lines_list = openAsList(inputpath)
+    #lines_list = ["7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1", "",  "22 13 17 11  0", " 8  2 23  4 24", "21  9 14 16  7", " 6 10  3 18  5", " 1 12 20 15 19", "", " 3 15  0  2 22", " 9 18 13 17  5", "19  8  7 25 23", "20 11 10 24  4", "14 21 16 12  6", "", "14 21 17 24  4", "10 16 15  9 19", "18  8 23 26 20", "22 11 13  6  5", " 2  0 12  3  7"]
+
+    numbers = numberline_to_array(lines_list[0])
+
+    bingo_felder = createBingoFelder(lines_list[2:])
+
+    if aufgabenteil == 'a':
+        gewinner = findWinnigBoard_a(bingo_felder, numbers)
+        # print(gewinner)
+        zahlenAufFeld = gewinner[0].position
+        letzte_zahl = gewinner[1]
+    else:
+        gewinner = findLastBoard(bingo_felder, numbers)
+        #print(gewinner)
+        zahlenAufFeld = gewinner["feld"].position
+        letzte_zahl = gewinner["zahl"]
+
+
+    print("Zahlen auf Bingofeld: {}".format(zahlenAufFeld.keys()))
+    gespielteZahlen = getGespielteZahlen(numbers, letzte_zahl)
+    print("Gespielte Zahlen: {}".format(gespielteZahlen))
+    for zahl in gespielteZahlen:
+        try:
+            zahlenAufFeld.pop(zahl)
+        except KeyError:
+            pass
+    offeneZahlen = sum(zahlenAufFeld.keys())
+    print("Noch offene Zahlen auf Bingofeld: {}".format(zahlenAufFeld.keys()))
+
+    print("offen: {}, zahl: {}".format(offeneZahlen, letzte_zahl))
+    return offeneZahlen * letzte_zahl
